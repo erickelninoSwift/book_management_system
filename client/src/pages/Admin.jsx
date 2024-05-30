fdc4541ea61a35b33c05b8a161c3c1c3af05b1d8
Erick El nino <erick.tshimbombo@yahoo.com>
2024-05-30 04:44:28 +0200

    woking on the layout of our dashboard

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -2,15 +2,15 @@ import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 import IconAdminImage from "../assets/600X600.jpg";
 const Admin = () => {
-  const { cookies, user } = useContext(UserContext);
+  const { cookies, chosenUser } = useContext(UserContext);
   const name = cookies.User;
-  console.log(user);
+
   return (
     <>
-      <div className="w-[75%] mx-auto my-5">
-        {name && (
+      <div className="w-[80%] mx-auto my-5">
+        {name && chosenUser && (
           <div className="flex">
-            <div className="max-h-fit max-w-fit flex flex-row">
+            <div className="max-h-fit max-w-[20%] flex flex-row">
               <button className="p-2 border-2 bg-white rounded-md border-gray-200 shadow-lg text-gray-500 focus:bg-teal-500 focus:outline-none focus:text-white absolute top-0 left-0 sm:hidden">
                 <svg
                   className="w-5 h-5 fill-current"
@@ -45,7 +45,7 @@ const Admin = () => {
                     />
                     <div>
                       <h2 className="font-medium text-xs md:text-sm text-center text-teal-500">
-                        {name}
+                        {name || chosenUser.name}
                       </h2>
                       <p className="text-xs text-gray-500 text-center">
                         Administrator
@@ -205,8 +205,99 @@ const Admin = () => {
               </div>
             </div>
 
-            <div className="bg-green-200 w-full ml-2 pl-2">
-              Everything will Come here{" "}
+            {/* Table */}
+            <div className="bg-green-200 w-full ml-5 pl-2">
+              <h3 class="mt-6 text-xl ml-5">Users</h3>
+              <div class="flex flex-col mt-6 mx-5">
+                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                  <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <div class="overflow-hidden border-b border-gray-200 rounded-md shadow-md">
+                      <table class="min-w-full overflow-x-scroll divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                          <tr>
+                            <th
+                              scope="col"
+                              class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                            >
+                              Name
+                            </th>
+                            <th
+                              scope="col"
+                              class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                            >
+                              Title
+                            </th>
+                            <th
+                              scope="col"
+                              class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                            >
+                              Status
+                            </th>
+                            <th
+                              scope="col"
+                              class="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                            >
+                              Role
+                            </th>
+                            <th scope="col" class="relative px-6 py-3">
+                              <span class="sr-only">Edit</span>
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                          <template x-for="i in 10">
+                            <tr class="transition-all hover:bg-gray-100 hover:shadow-lg">
+                              <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="flex items-center">
+                                  <div class="flex-shrink-0 w-10 h-10">
+                                    <img
+                                      class="w-10 h-10 rounded-full"
+                                      src="https://avatars0.githubusercontent.com/u/57622665?s=460&u=8f581f4c4acd4c18c33a87b3e6476112325e8b38&v=4"
+                                      alt=""
+                                    />
+                                  </div>
+                                  <div class="ml-4">
+                                    <div class="text-sm font-medium text-gray-900">
+                                      Ahmed Kamel
+                                    </div>
+                                    <div class="text-sm text-gray-500">
+                                      ahmed.kamel@example.com
+                                    </div>
+                                  </div>
+                                </div>
+                              </td>
+                              <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-900">
+                                  Regional Paradigm Technician
+                                </div>
+                                <div class="text-sm text-gray-500">
+                                  Optimization
+                                </div>
+                              </td>
+                              <td class="px-6 py-4 whitespace-nowrap">
+                                <span class="inline-flex px-2 text-xs font-semibold leading-5 text-green-800 bg-green-100 rounded-full">
+                                  Active
+                                </span>
+                              </td>
+                              <td class="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">
+                                Admin
+                              </td>
+                              <td class="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                                <a
+                                  href="#"
+                                  class="text-indigo-600 hover:text-indigo-900"
+                                >
+                                  Edit
+                                </a>
+                              </td>
+                            </tr>
+                          </template>
+                        </tbody>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         )}
