b42203559123e09ebb45b41f53f9597870b80f74
Erick El nino <erick.tshimbombo@yahoo.com>
2024-06-01 03:17:59 +0200

    added loader

--- client/src/pages/Allusers.jsx
+++ client/src/pages/Allusers.jsx
@@ -1,12 +1,53 @@
 import React, { useContext, useEffect, useState } from "react";
 import TableRow from "../components/TableRow";
 import { UserContext } from "../context/UserContext";
+import DataTable from "react-data-table-component";
+import { CircleLoader } from "react-spinners";
+import { FaRegTrashCan, FaPenToSquare } from "react-icons/fa6";
 const Allusers = () => {
   const { AlluserRegistered, setAllusersRegisered, cookies } =
     useContext(UserContext);
+  const [loading, setLoading] = useState(false);
+
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row._id.slice(0, 8),
+    },
+    {
+      name: "Name",
+      selector: (row) => row.name,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+    },
+    ,
+    {
+      name: "Phone",
+      selector: (row) => row.phone,
+    },
+    {
+      name: "Date",
+      selector: (row) => row.createdAt.slice(0, 10),
+    },
+    {
+      name: "Action",
+      selector: (row) => (
+        <>
+          <div className="flex gap-2">
+            <FaPenToSquare className="w-[20px] h-[20px] text-green-700" />
+            <FaRegTrashCan className="w-[20px] h-[20px] text-red-600" />
+          </div>
+        </>
+      ),
+    },
+  ];
 
   const [emptyMessage, setEmptyMessage] = useState(null);
+
   useEffect(() => {
+    setLoading(true);
     const handleFetchAllContact = async () => {
       const response = await fetch("http://localhost:8080/contacts", {
         method: "GET",
@@ -18,18 +59,36 @@ const Allusers = () => {
       const { success, contacts, norecord, errorFound } = data;
       if (!success && norecord) {
         setEmptyMessage(errorFound.toUpperCase());
+        setLoading(false);
       }
 
       setAllusersRegisered(() => contacts);
+      console.log(contacts);
       setEmptyMessage(null);
+      setLoading(false);
     };
     handleFetchAllContact();
   }, []);
   return (
-    <div className="bg-green-50 w-full ml-5 pl-2">
+    <div className="bg-green-50 w-full p-10">
       <h3 className="mt-6 text-xl ml-5">My Contacts</h3>
+      {loading ? (
+        <CircleLoader className="w-full h-[100%] mx-auto p-[20px] mt-[100px]" />
+      ) : (
+        <div className="mt-5">
+          {AlluserRegistered && (
+            <DataTable columns={columns} data={AlluserRegistered} pagination />
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
 
-      <div className="flex flex-col mt-6 mx-5">
+export default Allusers;
+
+{
+  /* <div className="flex flex-col mt-6 mx-5">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
             <div className="overflow-hidden border-b border-gray-200 rounded-md shadow-md">
@@ -79,9 +138,5 @@ const Allusers = () => {
             )}
           </div>
         </div>
-      </div>
-    </div>
-  );
-};
-
-export default Allusers;
+      </div> */
+}
