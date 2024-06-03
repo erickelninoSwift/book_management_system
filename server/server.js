4622fe430d4ae34f4e628b6ceef292d37a5759bd
Erick El nino <erick.tshimbombo@yahoo.com>
2024-06-03 02:42:52 +0200

    hotfix and done with this small project

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // ======================
 
